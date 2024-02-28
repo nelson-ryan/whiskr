d0394e1757ca764c8a9befe18074c910cedf35a7
Ryan Nelson <ryan@nelsonr.dev>
2024-02-27 19:12:35 -0700

    Add dynamic file-selection

--- whiskr.r
+++ whiskr.r
@@ -4,12 +4,15 @@ library(dplyr)
 library(googledrive)
 library(lubridate)
 library(tidyverse)
+library(purrr)
 
-history = drive_read_string("litter-robot_4_activity_2-25-2024.csv")
-history = readr::read_csv(history)
+history =
+    drive_ls() %>%
+    mutate(modified = map_chr(drive_resource, "modifiedTime")) %>%
+    filter(modified == max(modified)) %>% drive_read_string() %>%
+    readr::read_csv(col_name = TRUE)
 
 activityilst = c(
-    #"Cat Detected",
     "Pet Weight Recorded"
 )
 
@@ -25,12 +28,12 @@ history %>%
     mutate(Value = readr::parse_number(Value)) %>%
     filter(Value > 7) %>%
     select(Value, Timestamp) %>%
-    # group_by(Activity) %>%
-    # summarise(n = n()) %>%
     ggplot(
         aes(x = Timestamp, y = Value)
     ) +
     geom_point() +
     geom_smooth() + ggdark::dark_mode()
+
+ggsave('whiskr.png')
 
 
