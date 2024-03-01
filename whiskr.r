0dafdf26cf5e1cf02f7f2ad394c6096e6824bfcf
Ryan Nelson <ryan@nelsonr.dev>
2024-02-29 17:16:49 -0700

    Violining

--- whiskr.r
+++ whiskr.r
@@ -25,12 +25,15 @@ history %>%
             )
         )
     ) %>%
-    mutate(Value = readr::parse_number(Value)) %>%
+    mutate(
+        Value = readr::parse_number(Value)
+    ) %>%
     filter(Value > 7) %>%
     select(Value, Timestamp) %>%
     ggplot(
         aes(x = Timestamp, y = Value)
     ) +
+    ggtitle("Artemis: Weight over time") +
     geom_point() +
     geom_smooth() + ggdark::dark_mode()
 
@@ -47,12 +50,26 @@ history %>%
     ) %>%
     mutate(
         Time = hms::as_hms(Timestamp),
-        Week = factor(lubridate::week(Timestamp))
+        Week = lubridate::week(Timestamp)
     ) %>%
     ggplot(
-       aes(
-           y = Time,
-           x = Week
-       )
-   ) +
-     geom_violin() + ggdark::dark_mode()
+        aes(
+            y = Time,
+            x = factor(Week)
+        )
+    ) +
+    ggtitle("Artemis' Litter Box Visits throughout the Day, Grouped by Week") +
+    scale_x_discrete(name = "Week") +
+    scale_y_time(
+        breaks = c(
+            hms::as_hms("05:00:00"),
+            hms::as_hms("07:00:00"),
+            hms::as_hms("16:00:00"),
+            # hms::as_hms("18:15:00"),
+            # hms::as_hms("19:40:00"),
+            hms::as_hms("20:00:00")
+        ),
+        minor_breaks = NULL
+    ) +
+    geom_violin(adjust = .0001) +
+    ggdark::dark_mode()
